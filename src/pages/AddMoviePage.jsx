521706b9127738e8a449b519fcffc0cef947fc64
vijayg-mediwave <vijay_g@mindwaveventures.com>
2023-01-30 18:45:12 +0530

    add movie half done

--- src/pages/AddMoviePage.jsx
+++ src/pages/AddMoviePage.jsx
@@ -1,13 +1,131 @@
-import React, { useState } from "react";
+import React, { useMemo, useState, useEffect } from "react";
+import { useImmer } from "use-immer";
+import axios from "axios";
+
 import Page from "../components/Page";
 import Loading from "../pages/LoginPage";
 import Goback from "../components/Goback";
+import { apiAddMovie } from "../services/api/movie";
 
 const AddMoviePage = () => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const genres = ["Horror", "Scifi", "Romance", "Adventure", "Crime"];
+  const [newMovie, setNewMovie] = useImmer({
+    name: "",
+    //genre: "",
+    language: "",
+    yearOfRelease: 0,
+    genreList: [],
+    isLoading: false,
+    callApi: false,
+  });
+
+  const genres = ["Horror", "Sci-fi", "Romance", "Adventure", "Crime"];
   const languages = ["English", "Tamil", "Hindi", "French", "Telugu"];
+
+  const handleChange = ({ e, field }) => {
+    if (field === "name") {
+      setNewMovie((draft) => {
+        draft.name = e.target.value;
+      });
+    } else if (field === "genre") {
+      setNewMovie((draft) => {
+        draft.genreList.push(e.target.value);
+      });
+    } else if (field === "language") {
+      setNewMovie((draft) => {
+        draft.language = e.target.value;
+      });
+    } else if (field === "yearOfRelease") {
+      setNewMovie((draft) => {
+        draft.yearOfRelease = e.target.value;
+      });
+    }
+  };
+
+  const isValid = useMemo(() => {
+    if (
+      newMovie.name &&
+      newMovie.genreList &&
+      newMovie.language &&
+      newMovie.yearOfRelease
+    ) {
+      return true;
+    }
+    return false;
+  }, [
+    newMovie.name,
+    newMovie.genreList,
+    newMovie.language,
+    newMovie.yearOfRelease,
+  ]);
+
+  useEffect(() => {
+    const request = axios.CancelToken.source();
+
+    const doNewMovieSubmit = async () => {
+      setNewMovie((draft) => {
+        draft.isLoading = true;
+      });
+      try {
+        const { data } = await apiAddMovie({
+          payload: {
+            name: newMovie.name,
+            language: newMovie.language,
+            yearOfRelease: newMovie.yearOfRelease,
+            genreList: newMovie.genreList,
+          },
+          cancelToken: request.token,
+        });
+        resetnewMovie();
+      } catch (error) {
+        //setError(`failed to do login`);
+        console.log(error);
+      } finally {
+        setNewMovie((draft) => {
+          draft.isLoading = false;
+        });
+      }
+    };
+    if (
+      newMovie.callApi &&
+      newMovie.name &&
+      newMovie.genreList.length &&
+      newMovie.language &&
+      newMovie.yearOfRelease
+    ) {
+      doNewMovieSubmit();
+    }
+
+    return () => {
+      //console.log("component unmounted");
+      request.cancel();
+    };
+  }, [
+    newMovie.callApi,
+    newMovie.name,
+    newMovie.genreList.length,
+    newMovie.language,
+    newMovie.yearOfRelease,
+  ]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewMovie((draft) => {
+      draft.callApi = true;
+    });
+  };
+
+  const resetnewMovie = () => {
+    setNewMovie((draft) => {
+      draft.name = "";
+      draft.language = "";
+      draft.yearOfRelease = 0;
+      draft.genreList = [];
+      draft.isLoading = false;
+      draft.callApi = false;
+    });
+  };
 
   return (
     <Page title="Add new movie">
@@ -17,7 +135,7 @@ const AddMoviePage = () => {
 
       <h1>Add new movie</h1>
 
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="movie-name">
           Name
           <input
@@ -26,6 +144,8 @@ const AddMoviePage = () => {
             name="movie-name"
             placeholder="Movie name"
             required
+            value={newMovie.name}
+            onChange={(e) => handleChange({ e, field: "name" })}
           />
         </label>
 
@@ -37,6 +157,9 @@ const AddMoviePage = () => {
                 type="checkbox"
                 id={`checkbox-${g}`}
                 name={`checkbox-${g}`}
+                value={g}
+                onChange={(e) => handleChange({ e, field: "genre" })}
+                checked={newMovie.genreList.includes(g)}
               />
               {g}
             </label>
@@ -45,7 +168,13 @@ const AddMoviePage = () => {
 
         <label htmlFor="movie-lang">
           Language
-          <select id="movie-lang" name="movie-lang" required="">
+          <select
+            id="movie-lang"
+            name="movie-lang"
+            required=""
+            defaultValue={newMovie.language}
+            onChange={(e) => handleChange({ e, field: "language" })}
+          >
             <option disabled>Select…</option>
             {languages.map((l) => (
               <option key={l} value={l}>
@@ -63,10 +192,18 @@ const AddMoviePage = () => {
             name="movie-year"
             placeholder="Year of release"
             required
+            value={newMovie.yearOfRelease}
+            onChange={(e) => handleChange({ e, field: "yearOfRelease" })}
           />
         </label>
 
-        <button type="submit">Add movie</button>
+        <button
+          type="submit"
+          aria-busy={newMovie.isLoading}
+          disabled={!isValid}
+        >
+          Add movie
+        </button>
       </form>
     </Page>
   );
--- src/services/api/movie.js
+++ src/services/api/movie.js
@@ -3,3 +3,7 @@ import http from ".";
 export function apiGetMovieList({ cancelToken }) {
   return http.get("/movies", { cancelToken });
 }
+
+export function apiAddMovie({ payload, cancelToken }) {
+  return http.post("/movies", payload, { cancelToken });
+}
